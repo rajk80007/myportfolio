84438831cd4756e016c88ecf886225af5e2a0554
RK <rajk80007@gmail.com>
2024-06-07 23:23:27 +0530

    Logo inserted

--- app/_components/Navbar.jsx
+++ app/_components/Navbar.jsx
@@ -4,8 +4,12 @@ function Navbar() {
   return (
     <div>
         <div className="bg-gradient-to-r from-cyan-500 via-gray-500 to-blue-500 text-white h-14 p-5 flex justify-between items-center">
+        <div>
+          <h1>
+            <img src="/logo.png" alt="logo" width={60} height={60}/>
+            </h1>
         
-        <h1>Navbar</h1>
+        </div>
         <ul className="flex justify-center items-center gap-6">
             <li className='cursor-pointer p-3 hover:underline hover:bg-red-500'>Home</li>
             <li className='cursor-pointer p-3 hover:underline hover:bg-red-500'>About</li>
